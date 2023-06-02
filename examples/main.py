f51cba43d9707efecd8d34acb7cfdb46f5ea5788
cbdj <cldejessey@hotmail.fr>
2023-06-02 21:58:33 +0200

    update example

--- examples/main.py
+++ examples/main.py
@@ -1,78 +1,40 @@
 import pygame
+import simpleam
 pygame.init()
 
 screen = pygame.display.set_mode((1280, 720), pygame.SCALED | pygame.FULLSCREEN)
 clock = pygame.time.Clock()
-from ui import MenuButton # Ignore this file. It's for simplicity, and it has nothing to do with simpleam module
+from ui import MenuButton
 font = pygame.font.SysFont("Bold", 80)
-
-ANDROID = True
-try: 
-	import simpleam
-except: 
-	ANDROID = False
-
-class Callbacks(simpleam.RewardedCallbacks):
-	def __init__(self):
-		pass
-
-	def on_rewarded_loaded(self):
-		print("SIMPLEAM: Ad is loaded!")
-
-	def on_rewarded_opened(self):
-		print("SIMPLEAM: Ad is opened!")
-
-	def on_rewarded_started(self):
-		print("SIMPLEAM: Ad is started!")
-
-	def on_rewarded_closed(self):
-		print("SIMPLEAM: Ad closed!")
-
-	def on_rewarded_success(self, reward):
-		print(f"SIMPLEAM: Ad succesfully ended!")
-
-	def on_rewarded_left(self):
-		print("SIMPLEAM: Ad left application!")
-
-	def on_rewarded_load_fail(self, num):
-		print(f"SIMPLEAM: Ad failed to load: {num}")
-	
-if ANDROID:
-	simpleam.simpleam_init(None)
-	global banner, inter, rewarded
-	banner = simpleam.Banner(position="BOTTOM", size="SMART_BANNER")
-	inter = simpleam.Interstitial("ca-app-pub-3940256099942544/8691691433")
-	rewarded = simpleam.Rewarded()
-	rewarded.set_listener(Callbacks())
+simpleam.simpleam_init()
+global banner, inter, rewarded
+banner = simpleam.Banner(simpleam.TEST_BANNER_ID, "BOTTOM", "BANNER")
+banner.load_ad()
+inter = simpleam.Interstitial()
+rewarded = simpleam.Rewarded()
 
 def turn_banner():
 	global banner
-	if not banner.visible:
-		print("Turning on banner")
-		banner.load_ad()
-		banner.set_visibility(True)
-	else:
-		print("Turning off banner")
-		banner.set_visibility(False)
+	banner.set_visibility(not banner.get_visibility())
 
 
 def turn_interstitial():
-	global inter, banner
-	if inter:
-		inter.load_ad()
-		while not inter.is_loaded():
-			# Absolutely not recommended to do like this. This is used ONLY for example showcase!
-			pygame.time.delay(600)
-		inter.show()
+	global inter
+	inter.load_ad()
+	while inter.is_loading():
+		print('waiting for ad to load')
+		# Absolutely not recommended to do like this. This is used ONLY for example showcase!
+		pygame.time.delay(600)
+	inter.show()
 
 def turn_rewarded():
 	global rewarded
-	if rewarded:
-		rewarded.load_ad()
-		while not rewarded.is_loaded():
-			# Absolutely not recommended to do like this. This is used ONLY for example showcase! x2
-			pygame.time.delay(600)
-		rewarded.show()
+	rewarded.load_ad()
+	while rewarded.is_loading():
+		print('waiting for ad to load')
+		# Absolutely not recommended to do like this. This is used ONLY for example showcase! x2
+		pygame.time.delay(600)
+	rewarded.show()
 
 ui = []
 btn_surf = pygame.Surface((300, 140)).convert()
